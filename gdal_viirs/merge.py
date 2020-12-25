d0f8b32104760d1006600425bb65c54b1c5d3ba4
MaratBR <marat01q@gmail.com>
2020-12-25 14:26:32 +0700

    merge.py ошибки

--- gdal_viirs/merge.py
+++ gdal_viirs/merge.py
@@ -1,7 +1,9 @@
 import os
+from datetime import datetime
 from typing import List, Union, Tuple
 import numpy as np
 import rasterio
+import rasterio.crs
 from rasterio.merge import merge as _merge
 
 from gdal_viirs import utility
@@ -9,7 +11,7 @@ from gdal_viirs import utility
 MergeDataset = Union[rasterio.DatasetReader, str]
 
 
-def merge_files(datasets: List[MergeDataset], **kw) -> Tuple[np.ndarray, rasterio.Affine]:
+def merge_files(datasets: List[MergeDataset], **kw) -> Tuple[np.ndarray, rasterio.Affine, rasterio.crs.CRS]:
     open_datasets = []
     internally_open = []
     for ds in datasets:
@@ -20,18 +22,21 @@ def merge_files(datasets: List[MergeDataset], **kw) -> Tuple[np.ndarray, rasteri
         else:
             open_datasets.append(ds)
     try:
-        return _merge(open_datasets, **kw)
+        data, transform = _merge(open_datasets, **kw)
+        return data, transform, open_datasets[0].crs
     finally:
         for f in internally_open:
             f.close()
 
 
 def merge_files2tiff(datasets: List[MergeDataset], out_dir: str, filename=None, **kw):
-    merged, transform = merge_files(datasets, **kw)
+    merged, transform, crs = merge_files(datasets, **kw)
+    filename = filename or datetime.now().strftime('merged_%Y_%m_%d_%H%M%S.tiff')
     filepath = os.path.join(out_dir, filename)
     meta = utility.make_rasterio_meta(merged.shape[1], merged.shape[2], merged.shape[0])
     meta.update({
-        'transform': transform
+        'transform': transform,
+        'crs': crs
     })
-    with rasterio.open(filepath, 'w', **meta, **meta) as f:
+    with rasterio.open(filepath, 'w', **meta) as f:
         f.write(merged)
