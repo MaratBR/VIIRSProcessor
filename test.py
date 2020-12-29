54059d4fc0379d92747529165b02aedd2dbca75b
MaratBR <marat01q@gmail.com>
2020-12-29 16:01:20 +0700

    Удаление ненужных файлов

--- test.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from gdal_viirs.hl import NPPProcessor
-
-
-processor = NPPProcessor('/media/marat/Quack/Projects/GDAL_Data/NPP', '~/Documents')
-while True:
-    processor.process_recent()
-    time.sleep(3600)
